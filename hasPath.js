bcd52156544b4a15ccb45d123806b16f3ec91635
MizouziE <s.anglin@live.co.uk>
2022-01-05 14:14:54 +0000

    written and tested hasPath file & function

---
+++ hasPath.js
@@ -0,0 +1,24 @@
+const hasPath = (graph, src, dst) => {
+    if (src === dst) return true;
+
+    for (let neighbour of graph[src]) {
+       if (hasPath(graph, neighbour, dst) === true) {
+           return true;
+       } 
+    }
+
+    return false;
+};
+
+const graph = {
+    f: ['g', 'i'],
+    g: ['h'],
+    h: [],
+    i: ['g', 'k'],
+    j: ['i'],
+    k: []
+}
+
+console.log(hasPath(graph, 'f', 'k')); //true
+console.log(hasPath(graph, 'f', 'j')); //false
+console.log(hasPath(graph, 'i', 'h')); //true
