99888a0c3eff1d0728f69f71cfe6a1eba4d31ddc
MizouziE <s.anglin@live.co.uk>
2022-01-05 13:09:30 +0000

    graph fixed and method added

--- depthFirst.js
+++ depthFirst.js
@@ -1,9 +1,23 @@
+const depthFirstPrint = (graph, source) => {
+    const stack = [ source ];
+
+    while (stack.length > 0) {
+        const current = stack.pop();
+        console.log(current);
+
+        for (let neighbour of graph[current]) {
+            stack.push(neighbour);
+        }
+    }
+};
 
 const graph = {
-    a= ['b', 'c'],
-    b= ['d'],
-    c= ['e'],
-    d= ['f'],
-    e= [],
-    f= []
+    a: ['b', 'c'],
+    b: ['d'],
+    c: ['e'],
+    d: ['f'],
+    e: [],
+    f: []
 };
+
+depthFirstPrint(graph, 'a');
